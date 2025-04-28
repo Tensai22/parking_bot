d2e5ebf30172d3f347e6229cc87250e5dc0c94d5
Tensai <bakooofficial@gmail.com>
2025-04-28 20:33:01 +0500

    Second committ

--- config.py
+++ config.py
@@ -5,8 +5,4 @@ load_dotenv()
 print("Файл .env загружен") 
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-print(f"Загруженный токен: {BOT_TOKEN}")  
-
-DB_URL = os.getenv("DATABASE_URL")
-PARKING_API = os.getenv("PARKING_API")
-PAYMENT_API = os.getenv("PAYMENT_API")
+DATABASE_URL = os.getenv("DATABASE_URL")  # <-- исправлено
